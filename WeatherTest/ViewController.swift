72e489f7b13d217f11e4bba5cd93b8812eed5a36
ProstoTotti <prostototti@gmail.com>
2017-09-26 12:58:09 +0300

    git init

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Игорь Лисицкий on 26.09.17.
+//  Copyright © 2017 Igor Lisitzki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
